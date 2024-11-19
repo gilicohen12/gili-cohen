1f465d6bacc61b7dac9c6666f47b1bf8981661ed
Cohen <GCohen@mfs.com>
2024-11-19 11:36:21 -0500

    new project card UI

--- src/projects/ProjectCard.js
+++ src/projects/ProjectCard.js
@@ -1,105 +1,78 @@
 import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import { grey } from '@mui/material/colors';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import { DialogActions } from "@mui/material";
-import { grey } from '@mui/material/colors';
-
 
 export default function ProjectCard({ projectTitle, projectSummary, projectCodeURL, fullDetails, technology, image }) {
-  const [popout, setPopOut] = useState(false);
-  const handleClickOpen = () => {
-    setPopOut(true);
-  };
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleClose = () => {
-    setPopOut(false);
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
   };
 
   return (
-    <div>
-      <Card sx={{ width: 345, height: 420, backgroundColor: 'black', color: 'white', borderRadius: 4 }}>
-        <CardMedia
-          sx={{ height: 140, borderRadius: '4px 4px 0 0' }}
-          component="img"
-          alt={projectTitle}
-          height="300"
-          image= {image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {projectTitle}
-          </Typography>
-          <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap', marginBottom: 2, gap: '5px', alignItems: "left"}}>
-            {technology.map((tech, index) => (
-              <Chip key={index} label={tech.trim()} sx={{ backgroundColor: 'white', color: 'black', marginTop: "10px"}} />
-            ))}
-          </Stack>
-          <Typography variant="body2" sx={{ color: 'white' }}>
-            {projectSummary}
-          </Typography>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'flex-end', paddingRight: 2, alignItems: 'flex-end' }}>
-            <Button 
-            size="small" 
-            onClick={handleClickOpen} 
-            sx={{ 
-              '&:hover': {
-              backgroundColor: 'darkgrey',
-              }
-            }}
-            >
-            Learn More
-            </Button>
-        </CardActions>
-      </Card>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div key="front">
+        <Card sx={{ width: 345, height: 420, backgroundColor: 'black', color: 'white', borderRadius: 4}}>
+          <div>
+            <CardMedia
+              sx={{ height: 140, borderRadius: '4px 4px 0 0' }}
+              component="img"
+              alt={projectTitle}
+              height="300"
+              image={image}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {projectTitle}
+              </Typography>
+              <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap', marginBottom: 2, gap: '5px', alignItems: "left" }}>
+                {technology.map((tech, index) => (
+                  <Chip key={index} label={tech.trim()} sx={{ backgroundColor: 'white', color: 'black', marginTop: "10px" }} />
+                ))}
+              </Stack>
+              <Typography variant="body2" sx={{ color: 'white' }}>
+                {projectSummary}
+              </Typography>
+            </CardContent>
+          </div>
+          <CardActions sx={{ justifyContent: 'flex-end', paddingRight: 2, alignItems: 'flex-end' }}>
+            <Button onClick={handleFlip} sx={{ color: 'white' }}>Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
 
-      <Dialog
-        open={popout}
-        onClose={handleClose}
-        maxWidth='sm'
-        fullWidth={true}
-      >
-        <DialogTitle sx={{ fontSize: '1.5rem', color: 'black' }}>{projectTitle}</DialogTitle>
-        <DialogContent>
-            <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap', marginBottom: 2, gap: '10px' }}>
-            {technology.map((tech, index) => (
-              <Chip key={index} label={tech.trim()} sx={{ backgroundColor: 'black', color: 'white' }} />
-            ))}
-            </Stack>
-          <DialogContentText>
-            {fullDetails}
-          </DialogContentText>
-          
-        </DialogContent>
-        <DialogActions>
+      <div key="back">
+        <Card sx={{ width: 345, height: 420, backgroundColor: 'black', color: 'white', borderRadius: 4, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <CardContent sx={{ flexGrow: 1, marginTop: 3}}>
+            <Typography gutterBottom variant="h5" component="div">
+              {projectTitle}
+            </Typography>
+            <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap', marginBottom: 2, gap: '5px', alignItems: "left" }}>
+                {technology.map((tech, index) => (
+                  <Chip key={index} label={tech.trim()} sx={{ backgroundColor: 'white', color: 'black', marginTop: "10px" }} />
+                ))}
+              </Stack>
+            <Typography variant="body2" sx={{ color: 'white' }}>
+              {fullDetails}
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: 'flex-end', paddingRight: 2, alignItems: 'flex-end' }}>
+            <Button onClick={handleFlip} color="info" autoFocus sx={{ color: 'white' }}>Back</Button>
             {projectCodeURL &&
-            <Button 
-              onClick={() => window.open(projectCodeURL, '_blank')} 
-              color="info" 
-              autoFocus 
-              sx={{ 
-              backgroundColor: grey[900], 
-              color: 'white', 
-              '&:hover': {
-                backgroundColor: 'lightgrey',
-              }
-              }}
-            > 
-              View Code 
-            </Button>}
-          <Button onClick={handleClose} color="info" autoFocus> Close </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+              <Button 
+                onClick={() => window.open(projectCodeURL, '_blank')} 
+                color="info" autoFocus sx={{ backgroundColor: grey[900], color: 'white', '&:hover': { backgroundColor: 'lightgrey' } }}>View Code</Button>}
+          </CardActions>
+        </Card>
+      </div>
+    </ReactCardFlip>
   );
 }
